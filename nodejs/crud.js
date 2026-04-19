3bf5fe4036a9a8c5a127f271e07274b7b0c583b1
vikasthakurr <vikaskumar20012001@gmail.com>
2026-04-19 12:38:40 +0530

    feat: implement CRUD operations with authentication and add server boilerplate

---
+++ nodejs/crud.js
@@ -0,0 +1,133 @@
+import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const app = express();
+app.use(express.json());
+// console.log(mongoose);
+
+mongoose
+  .connect(
+    "mongodb+srv://vikaskumar20012001:Vikas123@dummy.oyfzhlq.mongodb.net/",
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  role: ["user", "admin"],
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.end("hi");
+});
+
+app.post("/register", async (req, res) => {
+  //   const { email, password } = req.body;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, password: hashedPassword });
+    await user.save();
+    res.end("user registered");
+  } catch (err) {
+    console.log(err);
+    res.end("something went wrong");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(404).json({ message: "please create a account first" });
+
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched)
+      return res.status(401).json({ message: "invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, "secretkey", { expiresIn: "1h" });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 3600000,
+    });
+    res.status(200).json({ message: "login successfull", token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+//verfication middleware for header
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token || !token.startsWith("Bearer"))
+    return res.status(401).json({ message: "unauthorized" });
+
+  const tokenValue = token.split(" ")[1];
+  try {
+    const isVerified = jwt.verify(tokenValue, "secretkey");
+    req.user = isVerified;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+app.get("/allusers", verifyToken, async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) return res.status(404).json({ message: "no users found" });
+    res.status(200).json({ users });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser)
+      return res.status(404).json({ message: "user not found" });
+    res.status(200).json({ message: "user deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+app.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email, password } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { email, password },
+      { new: true },
+    );
+    if (!updatedUser)
+      return res.status(404).json({ message: "user not found" });
+    res.status(200).json({ message: "user updated successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
